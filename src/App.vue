e77f89e6e0a221e67698871ad86df283e2baca21
kamari <kamnmclark@gmail.com>
2025-05-24 11:10:33 -0400

    switch to vue routing and move Header component to app-level

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,13 @@
 <template>
-  <SiteHeader title="Kamari Clark" />
-
-  <main>
-    <div class="container">
-      <section class="about">
-        <h2>## About Me</h2>
-        <UserMeta
-          currentEmployer="Self"
-          :skills="['C#', 'Python', 'Django', 'Kotlin', 'SQL', 'Vue.js', 'AWS']"
-        />
-        <p class="intro-text">
-          Learn about my code<br />
-          This is my programming site<br />
-          Nice to have you
-        </p>
-      </section>
-
-      <section class="projects">
-        <h2>## Projects</h2>
-        <ProjectCard
-          v-for="(p, i) in projects"
-          :key="i"
-          :title="p.title"
-          :github="p.github"
-          :thumbnailURL="p.thumbnailURL"
-          :docs="p.docs"
-          :desc="p.desc"
-          :quote="p.quote"
-          :tags="p.tags"
-        />
-      </section>
-    </div>
-  </main>
+  <Header />
+  <router-view />
 </template>
 
 <script>
-import SiteHeader from "./components/Header.vue";
-import UserMeta from "./components/UserInfoWindow.vue";
-import ProjectCard from "./components/ProjectCard.vue";
-
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: { SiteHeader, UserMeta, ProjectCard },
-  data() {
-    return {
-      projects: [
-        {
-          title: "Transit App",
-          github: "https://github.com/kamclark/transit-tracker/",
-          thumbnailURL: "./trains.jpg",
-          logo: "https://freerangestock.com/sample/123226/train-display-board-.jpg",
-          docs: "https://github.com/kamclark/transit-tracker/blob/main/README.md",
-          desc:
-            "Request proxmity-based updates for nearby train and bus route departures with customizable schedule metrics and severe delay warnings.",
-          quote: "More accurate than the official app!",
-          tags: ["Django", "Vue.js", "SQL", "AWS"],
-        },
-      ],
-    };
-  },
+    components: { Header }
+
 };
 </script>
-
-<style>
-@import "./style.css"; /* or break into component-scoped CSS */
-p:hover {
-  cursor: pointer;
-}
-</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <main>
+    <div class="container">
+      <section class="about">
+        <h2>## About Me</h2>
+        <UserMeta
+          currentEmployer="Self"
+          :skills="['C#', 'Python', 'Django', 'Kotlin', 'SQL', 'Vue.js', 'AWS']"
+        />
+        <p class="intro-text">
+          Learn about my code<br />
+          This is my programming site<br />
+          Nice to have you
+        </p>
+      </section>
+
+      <section class="projects">
+        <h2>## Projects</h2>
+        <ProjectCard
+          v-for="(p, i) in projects"
+          :key="i"
+          :title="p.title"
+          :github="p.github"
+          :thumbnailURL="p.thumbnailURL"
+          :docs="p.docs"
+          :desc="p.desc"
+          :quote="p.quote"
+          :tags="p.tags"
+        />
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import SiteHeader from "../components/Header.vue";
+import UserMeta from "../components/UserInfoWindow.vue";
+import ProjectCard from "../components/ProjectCard.vue";
+
+export default {
+  name: "HomeView",
+  components: { SiteHeader, UserMeta, ProjectCard },
+  data() {
+    return {
+      projects: [
+        {
+          title: "Transit App",
+          github: "https://github.com/kamclark/transit-tracker/",
+          thumbnailURL: "./trains.jpg",
+          logo: "https://freerangestock.com/sample/123226/train-display-board-.jpg",
+          docs: "https://github.com/kamclark/transit-tracker/blob/main/README.md",
+          desc:
+            "Request proxmity-based updates for nearby train and bus route departures with customizable schedule metrics and severe delay warnings.",
+          quote: "More accurate than the official app!",
+          tags: ["Django", "Vue.js", "SQL", "AWS"],
+        },
+      ],
+    };
+  },
+};
+</script>
